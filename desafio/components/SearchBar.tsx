071aa23bfc71acbd52037eae51ae1585560accda
ianzão developer <ian.dev.1993@gmail.com>
2025-02-08 00:47:35 -0300

    feat: add bg image searchsection

--- desafio/components/SearchBar.tsx
+++ desafio/components/SearchBar.tsx
@@ -1,17 +1,44 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Search, MapPin, Bed } from 'lucide-react';
 
 export default function SearchBar() {
   const [location, setLocation] = useState('');
   const [rooms, setRooms] = useState('');
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [filteredPlaces, setFilteredPlaces] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/Places")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Places recebidos:", data);
+        setPlaces(data);
+      })
+      .catch((error) => console.error("Erro ao buscar os places:", error));
+  }, []);
+
+  useEffect(() => {
+    if (location.trim() === '') {
+      setFilteredPlaces([]);
+      return;
+    }
+
+    const filtered = places
+      .filter((place) =>
+        place.Name.toLowerCase().includes(location.toLowerCase())
+      )
+      .map((place) => `${place.Name} - ${place.State.ShortName}`);
+
+    setFilteredPlaces(filtered);
+  }, [location, places]);
 
   return (
     <div className="flex items-center bg-white rounded-full shadow-md p-2 w-full max-w-2xl">
       <div className="flex items-center flex-1 px-4">
         <MapPin className="text-gray-500" size={20} />
-        <div className="ml-2">
+        <div className="ml-2 relative">
           <p className="text-xs font-bold text-gray-700">Localização</p>
           <input
             type="text"
@@ -20,6 +47,19 @@ export default function SearchBar() {
             onChange={(e) => setLocation(e.target.value)}
             className="outline-none text-sm bg-transparent w-full placeholder-gray-400 text-black"
           />
+          {filteredPlaces.length > 0 && (
+            <ul className="absolute mt-2 w-full bg-white border border-gray-300 shadow-lg max-h-40 overflow-auto z-10">
+              {filteredPlaces.map((place, index) => (
+                <li
+                  key={index}
+                  className="p-3 w-full text-black hover:bg-gray-100 cursor-pointer text-sm"
+                  onClick={() => setLocation(place)}
+                >              
+                  {place}               
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
       <div className="border-l border-gray-300 h-6"></div>
--- desafio/components/SearchSection.tsx
+++ desafio/components/SearchSection.tsx
@@ -1,12 +1,14 @@
 import SearchBar from "./SearchBar";
 
 export function SearchSection() {
-    return (
-      <section className="bg-gray-100 py-20 flex flex-col items-center justify-center text-center min-h-screen">
-          <div className="container mx-auto px-6 mt-16 flex flex-col items-center">
-            <h1 className="text-4xl font-bold text-gray-800 mb-6">Vende. Aluga. Conecta.</h1>
-            <SearchBar />
-          </div>
-      </section>
-    );
+  return (
+    <section
+      className="bg-gray-100 py-20 flex flex-col items-center justify-center text-center min-h-screen bg-[url('/comprar-alugar-vender.jpg')] bg-cover bg-center"
+    >
+      <div className="container mx-auto px-6 mt-16 flex flex-col items-center">
+        <h1 className="text-4xl font-semibold text-orange-500 mb-6">Vende. Aluga. Conecta.</h1>
+        <SearchBar />
+      </div>
+    </section>
+  );
 }
